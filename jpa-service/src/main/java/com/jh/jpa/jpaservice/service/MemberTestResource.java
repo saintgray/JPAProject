8edd68434162dadac972c0e893a4f2777fde3f49
OJH <saintgray0@gmail.com>
2022-07-19 22:34:40 +0900

    Collectors.groupingBy(Function func) Swagger API, swagger Annotation

--- jpa-service/src/main/java/com/jh/jpa/jpaservice/service/MemberTestResource.java
+++ jpa-service/src/main/java/com/jh/jpa/jpaservice/service/MemberTestResource.java
@@ -2,7 +2,15 @@ package com.jh.jpa.jpaservice.service;
 
 import com.jh.jpa.jpaservice.domain.spec.presentation.MemberListService;
 import com.jh.jpa.jpaservice.store.jpo.MemberJpo;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.models.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,8 +40,22 @@ public class MemberTestResource {
     }
 
     @GetMapping("/usersGroup/{groupingBy}")
-    public Map<String, List<MemberJpo>> getUsers(@PathVariable(name = "groupingBy") String groupingBy){
-        return service.getMembers(groupingBy);
+    @ApiOperation(
+            value = "회원정보 조회 (그룹)",
+            notes = "구독자별, 지역별로 그룹화한 회원들의 정보를 조회한다")
+    @ApiImplicitParams(
+            @ApiImplicitParam(name = "groupingBy", required = true, value = "그룹 기준(location, subscriber)"))
+    @ApiResponse(
+            code = 200, message = "success")
+    public ResponseEntity<?> getUsers(@PathVariable(name = "groupingBy") String groupingBy){
+        Map<String, List<MemberJpo>> usersGroup = service.getMembers(groupingBy);
+        ResponseEntity<?> result;
+        if(ObjectUtils.isEmpty(usersGroup)) {
+            result = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("올바른 그룹 기준을 선택하세요");
+        }else {
+            result = ResponseEntity.ok(usersGroup);
+        }
+        return result;
     }
 
     @GetMapping("/users/{searched}")
